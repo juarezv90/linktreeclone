a49213c47cf8004646e159be7bcacc24df19e3db
Victor Juarez <juarezv1990@gmail.com>
2023-02-23 16:33:32 -0500

    Initial Commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['projects.websetters.in'],
+  }
 }
 
 module.exports = nextConfig
